002ac5279cbaf9b4fb00166d52bea477c821a74a
oussamach54 <102917157+oussamach54@users.noreply.github.com>
2025-11-16 20:10:27 +0100

    Update urls.py

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from product import views
+from .views import (
+    OrdersCreateView, MyOrdersListView, OrderDetailView, OrderStatusAdminView,AdminOrdersListView,
+    AdminOrderDetailView,
+
+)
+
 
 urlpatterns = [
     # list/detail
@@ -24,4 +30,11 @@ urlpatterns = [
 
     # brands
     path("brands/", views.BrandsListView.as_view(), name="brands-list"),
+
+    path("orders/", OrdersCreateView.as_view(), name="orders-create"),              # POST create
+    path("orders/my/", MyOrdersListView.as_view(), name="orders-my"),              # GET my orders
+    path("orders/<int:pk>/", OrderDetailView.as_view(), name="orders-detail"),     # GET detail (owner/admin)
+    path("orders/<int:pk>/status/", OrderStatusAdminView.as_view(), name="orders-status"),  # PATCH status (admin)
+    path("orders/admin/", AdminOrdersListView.as_view(), name="orders-admin-list"),          # GET all (admin)
+    path("orders/admin/<int:pk>/", AdminOrderDetailView.as_view(), name="orders-admin-detail"),
 ]
